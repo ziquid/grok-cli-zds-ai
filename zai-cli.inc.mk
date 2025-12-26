818bb4999edaaee30c025704e26a475dcce8e9b9
Joseph Cheek <joseph@ziquid.com>
2025-12-26 14:50:01 -0700

    added pack-and-copy target to build, package, and copy to zds-ai project for inclusion in the container image.

--- zai-cli.inc.mk
+++ zai-cli.inc.mk
@@ -1,3 +1,8 @@
 FEATURES := bun
 
-ALL_TARGET := help
+ALL_TARGET := pack
+
+.PHONY: pack-and-copy
+pack-and-copy pc: pack ## package, copy to $(BUN_PACKAGE_NAME_STRIPPED).tgz and zds-ai project
+	cp $(BUN_PACKAGE_TARBALL) $(BUN_PACKAGE_NAME_STRIPPED).tgz
+	cp $(BUN_PACKAGE_NAME_STRIPPED).tgz ~/sca/zds-ai
